d6b9b308f3ad7013b4a10608a2802e1dd3ceee98
ramboe <yourmom@ramboe.io>
2025-09-08 10:11:47 +0200

    add map function

--- lua/helpers/init.lua
+++ lua/helpers/init.lua
@@ -1,3 +1,16 @@
+-- todo: put into ramboe utils
+-- map(): tiny wrapper around vim.keymap.set that lets you pass a string as the description.
+-- Usage:
+--   map("n", "K", vim.lsp.buf.hover, "Hover")
+--   map("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover" })
+-- Notes: keymaps are non-recursive by default; use `remap = true` if you want recursion.
+_G.map = function(mode, lhs, rhs, opts)
+  if type(opts) == "string" then
+    opts = { desc = opts }
+  end
+  return vim.keymap.set(mode, lhs, rhs, opts)
+end
+
 -- Highight the C# method
 function HighlightCSharpMethod()
   -- Jump to the beginning of the line
